886ed24c0aea545a272b3e6af7df3f95cec8dad4
Adam <adamboman1@hotmail.com>
2023-05-22 14:42:40 -0600

    copied button

--- src/components/Summarize.jsx
+++ src/components/Summarize.jsx
@@ -9,7 +9,7 @@ const Summarize = () => {
     summary: "",
   });
   const [allArticles, setAllArticles] = useState([]);
-
+  const [copied, setCopied] = useState("");
   const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
 
   useEffect(() => {
@@ -35,6 +35,12 @@ const Summarize = () => {
       setAllArticles(updatedAllArticles);
       localStorage.setItem("articles", JSON.stringify(updatedAllArticles));
     }
+  }
+
+  const handleCopy = (copyURL) => {
+    setCopied(copyURL);
+    navigator.clipboard.writeText(copyURL);
+    setTimeout(() => setCopied(false), 3000);
   }
 
   return (
@@ -71,8 +77,11 @@ const Summarize = () => {
         <div className='flex flex-col gap-1 max-h-60 overflow-y-auto'>
           {allArticles.map((item, index) => (
             <div key={`link-${index}`} onClick={() => setArticle(item)} className='link_card'>
-              <div className='copy_btn'>
-                <img src={copy} alt='copy_icon' className='w-[40%] h-[40%] object-contain'/>
+              <div className='copy_btn' onClick={() => {handleCopy(item.url)}}>
+                <img 
+                src={copied === item.url ? tick: copy} 
+                alt='copy_icon'
+                className='w-[40%] h-[40%] object-contain'/>
               </div>
               <p className='flex-1 font-satoshi text-blue-700 font-medium text-sm truncate'>
                 {item.url}
